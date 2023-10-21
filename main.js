10cfa4604661d65681cb474604994b6abf9942cb
Xalos95 <142462852+Xalos95@users.noreply.github.com>
2023-10-21 10:39:14 -0700

    created a functioning spin wheel

---
+++ main.js
@@ -0,0 +1,23 @@
+var play;
+
+var gameOptiond = {
+    //number of slices for the wheel
+    slices: 8,
+    
+    //amount of points for each slice
+    slicePoints: [
+        "500",
+        "600",
+        "700",
+        "800",
+        "900",
+        "1000",
+        "Bankrupt",
+        "Spin Again"
+    ],
+
+    //rotation time 
+    rotationTime: 5000
+}
+
+
